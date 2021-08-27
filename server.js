adab4c941a3041b8dbc67be1ef1d768a91487c7a
Diego <lima.j.diego@gmail.com>
2021-08-27 10:50:21 -0300

    configurando setup

---
+++ server.js
@@ -0,0 +1,18 @@
+/*
+ * Arquivo: server.js
+ * Description: 
+ * author: Diego Lima
+ * Data da criação: 27/08/2021 
+ */
+
+
+const express = require('express')
+const app = express
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+
+
